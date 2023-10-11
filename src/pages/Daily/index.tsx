f7281a8209b1a6a34b79475ccf16765694ffebd6
SidouGmr7 <goumirisidali@hotmail.com>
2023-10-12 00:16:45 +0100

    fix build errors

--- src/pages/Daily/index.tsx
+++ src/pages/Daily/index.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
-import { DailyList } from "./DailyList"
-import { DailyStat } from "./DailyStat"
-import { TabView, TabPanel } from "primereact/tabview"
+import { useState } from 'react'
+import { DailyList } from './DailyList'
+// import { DailyStat } from './DailyStat'
+import { TabView, TabPanel } from 'primereact/tabview'
 
 export const Daily = () => {
     const [activeIndex, setActiveIndex] = useState(0)
-    const [dataStat, setDataStat] = useState(null)
     return (
         <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} scrollable>
             <TabPanel header='Daily List'>
-                <DailyList setActiveIndex={setActiveIndex} setDataStat={setDataStat} />
+                <DailyList /*setActiveIndex={setActiveIndex} />
             </TabPanel>
             <TabPanel header='Daily Stat' disabled>
-                <DailyStat dataStat={dataStat} />
+                <DailyStat /*dataStat={dataStat}*/
+                />
             </TabPanel>
         </TabView>
     )
