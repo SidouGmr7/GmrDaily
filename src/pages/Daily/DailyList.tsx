9af2d14a3c28ae20f52d8d197c20e4a528be133f
SidouGmr7 <goumirisidali@hotmail.com>
2023-12-13 11:46:44 +0100

    fix import error

--- src/pages/Daily/DailyList.tsx
+++ src/pages/Daily/DailyList.tsx
@@ -1,7 +1,7 @@
 import { Toast } from 'primereact/toast'
 
 import { useToastModel } from '@/Resources/hooks/use-toast-modal'
-import TreeList from '@/Resources/components/treeList/TreeList'
+import TreeList from '@/Resources/components/TreeList'
 
 export const DailyList = () => {
     const { toast } = useToastModel()
